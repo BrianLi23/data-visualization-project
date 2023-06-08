bbb362defc70ae9e655deb09542ece1386b26015
BrianLi-23 <brian.li23@ycdsbk12.ca>
2023-06-08 01:33:19 -0400

    feat: created a list to contain data

--- src/cpt/listData.java
+++ src/cpt/listData.java
@@ -1,5 +1,14 @@
 package cpt;
 
+import java.io.*;
+import java.util.*;
+
 public class listData {
     
+    // Create array list of data
+    public static ArrayList<data> listofData = new ArrayList<data>();
+
+    public listData(){
+
+    }
 }
