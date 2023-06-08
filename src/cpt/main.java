bb1fbae72e783b3410b9ea2c96f62ca8d0042c2c
BrianLi-23 <brian.li23@ycdsbk12.ca>
2023-06-08 01:03:32 -0400

    feat: started method for reading data from csv file

--- src/cpt/main.java
+++ src/cpt/main.java
@@ -1,5 +1,29 @@
 package cpt;
 
-public class main {
+import java.io.*;
+import java.util.*;
+import java.lang.Math;
+
+public class main{
     
+    public static void readData() throws IOException{
+
+    // Text file input
+    BufferedReader reader = new BufferedReader(new FileReader("datasheet.csv"));
+    String strTextFile = "";
+
+    // Using while loop to read each line of text file, if readline returns null,
+    // end while loop
+    while (strTextFile != null) {
+        strTextFile = reader.readLine();
+        if (strTextFile != null) {
+            data(strTextFileMay)
+        }
+    }
+
+    // Executes the close method
+    reader.close();
 }
+
+}
+
