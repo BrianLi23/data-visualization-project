12464d5af7e1ca8df8f4dafc30154cab972d9f27
BrianLi-23 <brian.li23@ycdsbk12.ca>
2023-06-08 01:21:45 -0400

    feat: created function to read data and define in class

--- src/cpt/main.java
+++ src/cpt/main.java
@@ -5,25 +5,30 @@ import java.util.*;
 import java.lang.Math;
 
 public class main{
+
+    public static void main(String[] args) throws IOException {
+    }
     
     public static void readData() throws IOException{
 
     // Text file input
     BufferedReader reader = new BufferedReader(new FileReader("datasheet.csv"));
     String strTextFile = "";
+    String[] tempArray;
 
     // Using while loop to read each line of text file, if readline returns null,
     // end while loop
     while (strTextFile != null) {
         strTextFile = reader.readLine();
+        tempArray = strTextFile.split(",");
         if (strTextFile != null) {
-            data(strTextFileMay)
+            data datapoint = new data(tempArray[0], Integer.parseInt(tempArray[2]), Integer.parseInt(tempArray[3]));
         }
     }
 
     // Executes the close method
     reader.close();
-}
+    }
 
 }
 
