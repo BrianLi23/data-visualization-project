25acd060073a9ebc3bf6a611f8e475164d27cf47
BrianLi-23 <brian.li23@ycdsbk12.ca>
2023-06-09 15:25:31 -0400

    feat: added sort by year method (used merge sort)

--- src/cpt/main.java
+++ src/cpt/main.java
@@ -4,32 +4,33 @@ import java.io.*;
 import java.util.*;
 import java.lang.Math;
 
-public class main{
+public class main {
 
     public static void main(String[] args) throws IOException {
+
+        readData();
     }
-    
-    public static void readData() throws IOException{
 
-    // Text file input
-    BufferedReader reader = new BufferedReader(new FileReader("datasheet.csv"));
-    String strTextFile = "";
-    String[] tempArray;
+    public static void readData() throws IOException {
 
-    // Using while loop to read each line of text file, if readline returns null,
-    // end while loop
-    while (strTextFile != null) {
-        strTextFile = reader.readLine();
-        tempArray = strTextFile.split(",");
-        if (strTextFile != null) {
-            data datapoint = new data(tempArray[0], Integer.parseInt(tempArray[2]), Integer.parseInt(tempArray[3]));
-            listData.addData(datapoint);
+        // Text file input
+        BufferedReader reader = new BufferedReader(new FileReader("datasheet.csv"));
+        String strTextFile = "";
+        String[] tempArray;
+
+        // Using while loop to read each line of text file, if readline returns null,
+        // end while loop
+        while (strTextFile != null) {
+            strTextFile = reader.readLine();
+            tempArray = strTextFile.split(",");
+            if (strTextFile != null) {
+                data datapoint = new data(tempArray[0], Integer.parseInt(tempArray[2]), Integer.parseInt(tempArray[3]));
+                listData.addData(datapoint);
+            }
         }
-    }
 
-    // Executes the close method
-    reader.close();
+        // Executes the close method
+        reader.close();
     }
 
 }
-
