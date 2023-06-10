db9ef8fbc39203bd966452e68663236c44ff3ce7
BrianLi-23 <brian.li23@ycdsbk12.ca>
2023-06-10 02:47:13 -0400

    feat: continued, changed data sheet

--- src/charts/BarChartApp.java
+++ src/charts/BarChartApp.java
@@ -10,6 +10,7 @@ import javafx.scene.chart.BarChart;
 import javafx.scene.chart.CategoryAxis;
 import javafx.scene.chart.NumberAxis;
 import javafx.stage.Stage;
+import javafx.scene.chart.StackedAreaChart;
 
 /**
  * A chart that displays rectangular bars with heights indicating data values
@@ -18,29 +19,40 @@ import javafx.stage.Stage;
  */
 public class BarChartApp extends Application {
 
-  private BarChart chart;
-  private CategoryAxis xAxis;
+  private StackedAreaChart chart;
+  private NumberAxis xAxis;
   private NumberAxis yAxis;
 
   public Parent createContent() {
-    String[] years = { "2007", "2008", "2009" };
-    xAxis = new CategoryAxis();
-    xAxis.setCategories(FXCollections.<String>observableArrayList(years));
-    yAxis = new NumberAxis("Units Sold", 0.0d, 3000.0d, 1000.0d);
-    ObservableList<BarChart.Series> barChartData = FXCollections.observableArrayList(
-        new BarChart.Series("Apples", FXCollections.observableArrayList(
-            new BarChart.Data(years[0], 567d),
-            new BarChart.Data(years[1], 1292d),
-            new BarChart.Data(years[2], 1292d))),
-        new BarChart.Series("Lemons", FXCollections.observableArrayList(
-            new BarChart.Data(years[0], 956),
-            new BarChart.Data(years[1], 1665),
-            new BarChart.Data(years[2], 2559))),
-        new BarChart.Series("Oranges", FXCollections.observableArrayList(
-            new BarChart.Data(years[0], 1154),
-            new BarChart.Data(years[1], 1927),
-            new BarChart.Data(years[2], 2774))));
-    chart = new BarChart(xAxis, yAxis, barChartData, 25.0d);
+    xAxis = new NumberAxis("X Values", 1.0d, 9.0d, 2.0d);
+    yAxis = new NumberAxis("Y Values", 0.0d, 30.0d, 2.0d);
+
+    ObservableList<StackedAreaChart.Series> areaChartData = FXCollections.observableArrayList(
+        new StackedAreaChart.Series("Series 1",
+            FXCollections.observableArrayList(
+                new StackedAreaChart.Data(0, 4),
+                new StackedAreaChart.Data(2, 5),
+                new StackedAreaChart.Data(4, 4),
+                new StackedAreaChart.Data(6, 2),
+                new StackedAreaChart.Data(8, 6),
+                new StackedAreaChart.Data(10, 8))),
+        new StackedAreaChart.Series("Series 2",
+            FXCollections.observableArrayList(
+                new StackedAreaChart.Data(0, 8),
+                new StackedAreaChart.Data(2, 2),
+                new StackedAreaChart.Data(4, 9),
+                new StackedAreaChart.Data(6, 7),
+                new StackedAreaChart.Data(8, 5),
+                new StackedAreaChart.Data(10, 7))),
+        new StackedAreaChart.Series("Series 3",
+            FXCollections.observableArrayList(
+                new StackedAreaChart.Data(0, 2),
+                new StackedAreaChart.Data(2, 5),
+                new StackedAreaChart.Data(4, 8),
+                new StackedAreaChart.Data(6, 6),
+                new StackedAreaChart.Data(8, 9),
+                new StackedAreaChart.Data(10, 7))));
+    chart = new StackedAreaChart(xAxis, yAxis, areaChartData);
     return chart;
   }
 
